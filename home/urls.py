7998a07e02f8a8e20a8b914ecd5def2d33883ece
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-07 17:54:31 +0000

    Wire up templates

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from home.views import Home
+from home.views import Home, About, Contact
+
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("about", About.as_view(), name="about"),
+    path("contact", Contact.as_view(), name="contact")
 ]
