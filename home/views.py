bafae595f36d87de13cf764163147a836092dd97
cafalchio <mcafalchio@gmail.com>
2023-09-07 19:32:59 +0100

    hide api key

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.base import TemplateView
+from django.conf import settings
 
 
 class Home(TemplateView):
     template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView, self).get_context_data(**kwargs)
+        context['mapbox_key'] = settings.MAPBOX_KEY
+        return context
 
 
 class About(TemplateView):
